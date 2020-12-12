e65e783f5268e7126944d22cd8817a0553653638
chikondi <chikondiman@gmail.com>
2020-12-12 01:08:49 -0600

    CRUD

--- app/controllers/api/intentions_controller.rb
+++ app/controllers/api/intentions_controller.rb
@@ -3,21 +3,6 @@ class Api::IntentionsController < ApplicationController
     @goals = Goal.all
     # render "index.json.jb"
 
-    p "THIS IS RIGHT BEFORE CURRENT USER"
-    p current_user
-    p "THIS IS RIGHT AFTER CURRENT USER"
-
-    if params[:search]
-      @goals = Goal.where("name LIKE ?", "%#{params[:search]}%")
-    else
-      @goals = Goal.all
-    end
-
-    if current_user
-      @goals = Goal.all
-    else
-      @goals = Goal.all
-    end
     render "index.json.jb"
   end
 
@@ -29,11 +14,13 @@ class Api::IntentionsController < ApplicationController
 
   def create
     @goal = Goal.new(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      email: params[:email],
-      phone_number: params[:phone_number],
-      address: params[:address],
+      intention: params[:intention],
+      goalKeeper: params[:goalKeeper],
+      startTime: params[:startTime],
+      endTime: params[:endTime],
+      completed: params[:completed],
+      goalCohort: params[:goalCohort],
+
     )
 
     if @goal.save
@@ -46,12 +33,12 @@ class Api::IntentionsController < ApplicationController
   def update
     the_id = params[:id]
     @goal = Goal.find_by(id: the_id)
-    @goal.first_name = params[:first_name] || @goal.first_name
-    @goal.last_name = params[:last_name] || @goal.last_name
-    @goal.middle_name = params[:middle_name] || @goal.middle_name
-    @goal.email = params[:email] || @goal.email
-    @goal.phone_number = params[:phone_number] || @goal.phone_number
-    @goal.address = Geocoder.coordinates(params[:address]) || @contact.address
+    @goal.intention = params[:intention] || @goal.intention
+    @goal.goalKeeper = params[:goalKeeper] || @goal.goalKeeper
+    @goal.startTime = params[:startTime] || @goal.startTime
+    @goal.endTime = params[:endTime] || @goal.endTime
+    @goal.completed = params[:completed] || @goal.completed
+    @goal.goalCohort = params[:goalCohort] || @goal.goalCohort
 
     if @contact.save
       render "show.json.jb"
