14fa30e49cd2dd44c11cdfdbf362c94bc90fc69b
chikondi <chikondiman@gmail.com>
2020-12-13 08:47:35 -0500

    Index & Show Json Page addition

--- app/controllers/api/intentions_controller.rb
+++ app/controllers/api/intentions_controller.rb
@@ -40,10 +40,10 @@ class Api::IntentionsController < ApplicationController
     @goal.completed = params[:completed] || @goal.completed
     @goal.goalCohort = params[:goalCohort] || @goal.goalCohort
 
-    if @contact.save
+    if @goal.save
       render "show.json.jb"
     else
-      render json: { errors: @contact.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @goal.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
